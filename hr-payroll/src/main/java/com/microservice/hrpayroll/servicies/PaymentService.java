aedaf639fbb2730836074f5b8da73ca5dbe709ff
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-02-25 17:21:29 -0300

    Payment resource, entity, service
    Payment resource, entity, service

---
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/servicies/PaymentService.java
@@ -0,0 +1,14 @@
+package com.microservice.hrpayroll.servicies;
+
+import org.springframework.stereotype.Service;
+
+import com.microservice.hrpayroll.entities.Payment;
+
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
