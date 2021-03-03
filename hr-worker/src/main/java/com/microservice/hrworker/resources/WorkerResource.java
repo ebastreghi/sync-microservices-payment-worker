3727480e93744f2069848feb90eed38ed55bdd01
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-03-03 15:31:58 -0300

    Pointing microservices for the ConfigServer
    Pointing microservices for the ConfigServer

--- hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/microservice/hrworker/resources/WorkerResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ import com.microservice.hrworker.repositories.WorkerRepository;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
-
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
